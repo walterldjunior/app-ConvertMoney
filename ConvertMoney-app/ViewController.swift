c26a84ed3b43d49020e48ee5336ce201bcd26a1e
Walter Junior <juniordeonisio@gmail.com>
2019-02-14 10:58:40 -0200

    Initial Commit

---
+++ ConvertMoney-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertMoney-app
+//
+//  Created by WalterJunior on 2/14/19.
+//  Copyright © 2019 WalterLdJunior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
